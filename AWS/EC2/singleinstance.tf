b0a02b6ccfd9520c89d7ed7c112d71f6836da166
darshan <dar.raul@gmail.com>
2019-04-27 19:30:23 +0530

    single instance

---
+++ AWS/EC2/singleinstance.tf
@@ -0,0 +1,12 @@
+/* NOT  USING FOR AWS AS WILL PREFER SYSTEM ONES 
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+*/
+
+resource "aws_instance" "example" {
+  ami           = "ami-007d5db58754fa284"
+  instance_type = "t2.micro"
+}
