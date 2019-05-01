c77d77e2d8d34f834acee3a716c51d5742eefc4f
darshan <dar.raul@gmail.com>
2019-05-01 06:14:06 +0530

    iamuser in group

--- AWS/IAM/iam.tf
+++ AWS/IAM/iam.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_iam_user" "dr" {
   name = "darshanraul"
   
@@ -27,4 +28,28 @@ resource "aws_iam_user_policy" "dr_ro" {
   ]
 }
 EOF
+}
+*/
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    "${aws_iam_user.user_one.name}",
+    "${aws_iam_user.user_two.name}",
+  ]
+
+  group = "${aws_iam_group.group.name}"
+}
+
+resource "aws_iam_group" "group" {
+  name = "test-group"
+}
+
+resource "aws_iam_user" "user_one" {
+  name = "test-user"
+}
+
+resource "aws_iam_user" "user_two" {
+  name = "test-user-two"
 }
